06fca4bc2d692689a65501a68df56f8f8ab7ec06
0xC137 <sami.ezzerouali@hotmail.com>
2025-07-14 22:41:53 +0200

    Updated page.tsx

--- components/dashboard/stats-cards.tsx
+++ components/dashboard/stats-cards.tsx
@@ -1,5 +1,6 @@
 @@ .. @@
    return (
+   )
 -    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-6">
 +    <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-6 gap-3 sm:gap-4 lg:gap-6">
        {stats.map((stat) => (
@@ -20,3 +21,6 @@
      </div>
    );
  }
+)
+)
+}
